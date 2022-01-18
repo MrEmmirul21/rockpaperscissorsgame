070ed41b4b5c247e768ec72fdc548d0e22eaa9e6
hasmishx <93893827+hasmishx@users.noreply.github.com>
2022-01-19 00:08:52 +0800

    ni client dia

---
+++ cgame.py
@@ -0,0 +1,51 @@
+import socket
+import threading
+
+def game():
+    print(" ██████╗░░█████╗░░█████╗░██╗░░██╗  ██████╗░░█████╗░██████╗░███████╗██████╗░")
+    print(" ██╔══██╗██╔══██╗██╔══██╗██║░██╔╝  ██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗")
+    print(" ██████╔╝██║░░██║██║░░╚═╝█████═╝░  ██████╔╝███████║██████╔╝█████╗░░██████╔╝")
+    print(" ██╔══██╗██║░░██║██║░░██╗██╔═██╗░  ██╔═══╝░██╔══██║██╔═══╝░██╔══╝░░██╔══██╗")
+    print(" ██║░░██║╚█████╔╝╚█████╔╝██║░╚██╗  ██║░░░░░██║░░██║██║░░░░░███████╗██║░░██║") 
+    print(" ╚═╝░░╚═╝░╚════╝░░╚════╝░╚═╝░░╚═╝  ╚═╝░░░░░╚═╝░░╚═╝╚═╝░░░░░╚══════╝╚═╝░░╚═╝")
+    print(" ░██████╗░█████╗░██╗░██████╗░██████╗░█████╗░██████╗░")
+    print(" ██╔════╝██╔══██╗██║██╔════╝██╔════╝██╔══██╗██╔══██╗")
+    print(" ╚█████╗░██║░░╚═╝██║╚█████╗░╚█████╗░██║░░██║██████╔╝")
+    print(" ░╚═══██╗██║░░██╗██║░╚═══██╗░╚═══██╗██║░░██║██╔══██╗")
+    print(" ██████╔╝╚█████╔╝██║██████╔╝██████╔╝╚█████╔╝██║░░██║")
+    print(" ╚═════╝░░╚════╝░╚═╝╚═════╝░╚═════╝░░╚════╝░╚═╝░░╚═╝")
+    print(" ###  Welcome to Rock Paper Scissor Multiplayer Game!  ###")
+    print(" There will be FIVE rounds of game, Your choices >>> ")
+    print(" >>> r = Rock ")
+    print(" >>> s = Scissor ")
+    print(" >>> p = Paper ")
+    choice = input(" What is your choice ? >> ")
+    if choice == "r" or choice == "s" or choice == "p":
+        client.send(choice.encode("utf-8"))
+        print("Waiting for other player...")
+        result = s.recv(1024).decode("utf-8")
+        print(result)
+        client.close()
+    else:
+        print("Not correct input...")
+        game()
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = '192.168.56.103'
+port = 8888
+try:
+    client.connect((host, port))
+    print(" Waiting for other player...")
+except socket.error as e:
+    print(str(e))
+    
+ready_to_play = False
+
+while True:
+    dataIn = s.recv(1024).decode("utf-8")
+    if not ready_to_play:
+        if dataIn == "READY_TO_PLAY":
+            print("Ready to play")
+            game()
+            ready_to_play = True
+        
