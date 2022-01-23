0b8993ada41eb8b4070172647df9322085f77baf
hasmishx <93893827+hasmishx@users.noreply.github.com>
2022-01-23 16:19:16 +0800

    final version

--- sgame.py
+++ sgame.py
@@ -1,10 +1,9 @@
 import socket
 import threading
-import time
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = ''
-port = 5050
+port = 8888
 s.bind((host, port))
 s.listen(3)
 print("Waiting for another player to join")
@@ -23,41 +22,43 @@ def sendtoall(data):
     for player in players:
         player.send(data.encode("utf-8"))
 
-def games():
+def game():
     global player1, player2
-    if player1 == "r":
-        if player2 == "s":
-            sendto1(" You win :)", players[0])
-            sendto1(" You Lose :(", players[1])
-        elif player2 == "p":
-            sendto1(" You Lose :(", players[0])
-            sendto1(" You win :)", players[1])
+    if player1 != "" and player2 != "":
+        if player1 == "r":
+            if player2 == "r":
+                sendtoall(" It's a Draw!")
+            elif player2 == "s":
+                sendto1(" You Win :)", players[0])
+                sendto1(" You Lose :(", players[1])
+            elif player2 == "p":
+                sendto1(" You Lose :(", players[0])
+                sendto1(" You Win :)", players[1])
                 
-    elif player1 == "s":
-        if player2 == "p":
-            sendto1(" You win :)", players[0])
-            sendto1(" You Lose :(", players[1])
-        elif player2 == "r":
-            sendto1(" You Lose :(", players[0])
-            sendto1(" You win :)", players[1])
+        elif player1 == "s":
+            if player2 == "s":
+                sendtoall(" It's a Draw!")
+            elif player2 == "p":
+                sendto1(" You Win :)", players[0])
+                sendto1(" You Lose :(", players[1])
+            elif player2 == "r":
+                sendto1(" You Lose :(", players[0])
+                sendto1(" You Win :)", players[1])
                 
-    elif player1 == "p":
-        if player2 == "r":
-            sendto1(" You win :)", players[0])
-            sendto1(" You Lose :(", players[1])
-        elif player2 == "s":
-            sendto1(" You Lose :(", players[0])
-            sendto1(" You win :)", players[1])
-            
-    elif player1 == player2:
-        sendtoall("IT'S A DRAW!!!")
+        elif player1 == "p":
+            if player2 == "p":
+                sendtoall(" It's a Draw!")
+            elif player2 == "r":
+                sendto1(" You Win :)", players[0])
+                sendto1(" You Lose :(", players[1])
+            elif player2 == "s":
+                sendto1(" You Lose :(", players[0])
+                sendto1(" You Win :)", players[1])
         player1 = ""
         player2 = ""
-        time.sleep(0.5)
-        sendtoall("reset")
 
 def thread_handling(conn, currentPlayerID):
-    while True:
+    for x in range(5):
         try:
             choice = conn.recv(1024).decode("utf-8")
             global player1, player2
@@ -65,10 +66,10 @@ def thread_handling(conn, currentPlayerID):
                 if choice == "r" or choice == "s" or choice == "p":
                     if currentPlayerID == 0:
                         player1 = choice
-                        games()
+                        game()
                     if currentPlayerID == 1:
                         player2 = choice
-                        games()
+                        game()
                 else:
                     conn.send("\nError: Invalid input.".encode("utf-8"))
         except:
@@ -85,10 +86,9 @@ while True:
         players.append(conn)
         thread = threading.Thread(target=thread_handling, args=(conn, playerID))
         thread.start()
-        
         if len(players) == 2:
             sendtoall("ready")
-
         playerID += 1
     else:
+        print("Can't have more players...")
         break
